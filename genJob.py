2f064b1075bf9a1493ae66b02536ab0fc9bceeaa
MeaningLessss <huangchen1999@gmail.com>
2024-02-02 19:54:52 +0800

    test

--- genJob.py
+++ genJob.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 
 slice = [1, 2, 4, 8]
-dist = [0.3, 0.6, 0.8]
+dist = [0.25, 0.5, 0.75]
 batchSize = 1
 
 for k in range(0, 5):
@@ -10,7 +10,7 @@ for k in range(0, 5):
     njob = 1000
     fname = "workload/load" + str(k) + ".txt"
     f = open(fname, "w")
-    t = np.random.poisson(3.4 - k / 5, njob)
+    t = np.random.poisson(7 - k / 5, njob)
     for i in range(1, len(t)):
         t[i] = t[i-1] + t[i]
     for i in range(0, len(t)):
@@ -33,3 +33,39 @@ for k in range(0, 5):
         f.write(str(int(t[i])) + " " + ' '.join(str(rt) for rt in runtime) + " " + str(slice[sliceIdx]) + "\n")
 
     f.close()
+
+# import numpy as np
+# import random
+
+# slice = [1, 2, 4, 8]
+# dist = [0.25, 0.5, 0.75]
+# batchSize = 1
+
+# for k in range(0, 5):
+#     t = []
+#     njob = 1000
+#     fname = "workload/load" + str(k) + ".txt"
+#     f = open(fname, "w")
+#     t = np.random.poisson(3.4 - k / 5, njob)
+#     for i in range(1, len(t)):
+#         t[i] = t[i-1] + t[i]
+#     for i in range(0, len(t)):
+#         t[i] = t[i - i % batchSize]
+
+#     for i in range(njob):
+#         x = random.random()
+#         sliceIdx = 3
+#         if x < dist[0]:
+#             sliceIdx = 0
+#         elif x < dist[1]:
+#             sliceIdx = 1
+#         elif x < dist[2]:
+#             sliceIdx = 2
+#         seq = random.randint(20,200)
+#         para = random.randint(20,200)
+#         runtime = []
+#         for j in slice:
+#             runtime.append(seq + para // j)
+#         f.write(str(int(t[i])) + " " + ' '.join(str(rt) for rt in runtime) + " " + str(slice[sliceIdx]) + "\n")
+
+#     f.close()
