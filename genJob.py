43daaf7ae940ffc65f27b9b7acbffd995fee2203
MeaningLessss <huangchen1999@gmail.com>
2024-02-15 16:54:32 +0800

    modify util

--- genJob.py
+++ genJob.py
@@ -5,31 +5,31 @@ slice = [1, 2, 4, 8]
 dist = [0.25, 0.5, 0.75]
 batchSize = [1, 20, 40]
 
-for k in range(0, 3):
-    t = []
-    njob = 1000
-    fname = "workload/load" + str(k) + ".txt"
-    f = open(fname, "w")
-    t = np.random.poisson(3.0, njob)
-    for i in range(1, len(t)):
-        t[i] = t[i-1] + t[i]
-    for i in range(0, len(t)):
-        t[i] = t[i - i % batchSize[k] + batchSize[k] // 2]
+for k in range(0, 15):
+        t = []
+        njob = 1000
+        fname = "workload/load" + str(k) + ".txt"
+        f = open(fname, "w")
+        t = np.random.poisson(3.0, njob)
+        for i in range(1, len(t)):
+            t[i] = t[i-1] + t[i]
+        for i in range(0, len(t)):
+            t[i] = t[i - i % batchSize[k//5] + batchSize[k//5] // 2]
 
-    for i in range(njob):
-        x = random.random()
-        sliceIdx = 3
-        if x < dist[0]:
-            sliceIdx = 0
-        elif x < dist[1]:
-            sliceIdx = 1
-        elif x < dist[2]:
-            sliceIdx = 2
-        seq = random.randint(10,100)
-        para = random.randint(50,500)
-        runtime = []
-        for j in slice:
-            runtime.append(seq + para // j)
-        f.write(str(int(t[i])) + " " + ' '.join(str(rt) for rt in runtime) + " " + str(slice[sliceIdx]) + "\n")
+        for i in range(njob):
+            x = random.random()
+            sliceIdx = 3
+            if x < dist[0]:
+                sliceIdx = 0
+            elif x < dist[1]:
+                sliceIdx = 1
+            elif x < dist[2]:
+                sliceIdx = 2
+            seq = random.randint(10,100)
+            para = random.randint(50,500)
+            runtime = []
+            for j in slice:
+                runtime.append(seq + para // j)
+            f.write(str(int(t[i])) + " " + ' '.join(str(rt) for rt in runtime) + " " + str(slice[sliceIdx]) + "\n")
 
-    f.close()
+        f.close()
--- util.py
+++ util.py
@@ -2,29 +2,32 @@ import numpy as np
 
 ALGO = ["bf", "sp", "wf"]
 
-for k in range(0, 5):
+for k in range(0, 3):
     print("load" + str(k) + ":")
-    T = 1e9
-    utils = []
-    for algo in ALGO:
-        fname = "util/load" + str(k) + "_" + algo  + ".txt"
-        f = open(fname, "r")
-        last_util = 0.0
-        last_time = 0
-        util = []
-        for x in f:
-            x = x.split()
-            if len(x) == 1:
-                T = int(x[0])
-            else:
-                t = int(x[0]) 
-                for i in range(last_time, t):
-                    util.append(last_util)
-                last_time = t
-                last_util = float(x[1])
-        utils.append(util)
-        f.close()
+    utils = [0.0, 0.0, 0.0]
+    for n in range(0, 5):
+        T = 1e9
+        for idx, algo in enumerate(ALGO):
+            fname = "util/load" + str(k*5 + n) + "_" + algo  + ".txt"
+            f = open(fname, "r")
+            last_util = 0.0
+            last_time = 0
+            util = []
+            for x in f:
+                x = x.split()
+                if len(x) == 1:
+                    T = int(x[0])
+                else:
+                    t = int(x[0]) 
+                    for i in range(last_time, t):
+                        util.append(last_util)
+                    last_time = t
+                    last_util = float(x[1])
+            # utils.append(util)
+            util = util[:T]
+            utils[idx] += sum(util) / len(util)
+            f.close()
 
     for i in range(len(ALGO)):
-        util = utils[i][:T]
-        print("\t", ALGO[i], ": ", sum(util) / len(util))
+        util = utils[i] / 5
+        print("\t", ALGO[i], ": ", util)
